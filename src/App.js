afbf2040e15e521f8e887f3f4744a58c84d9a700
dipanshu283 <en20cs301137@medicaps.ac.in>
2021-12-22 20:25:01 +0530

    Appjsxupdate

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './component/ChatFeed';
+// import LoadingForm from './component/LoadingForm';
 import './App.css';
 
-function App() {
-  return (
-    <>
-    <div className="App">
-   <h1>hello react</h1>
-   <p>this is para for testing update check up</p>
-   <p>hello agian</p>
-   <p>hehe hello</p>
-    </div>
-    </>
-  );
+
+export function App() {
+
+	// if(!localStorage.getItem('username')) return <LoadingForm/>
+	return (
+		<ChatEngine
+		height='100vh'
+		userName={localStorage.getItem('username')}
+		userSecret={localStorage.getItem('secret')}
+		projectID='22d4bae1-7b87-429d-9ce8-c98bc7100c44'
+      renderChatFeed={(chatAppState) => <ChatFeed {...chatAppState} />}
+	  onNewMessage = {()=> new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+		/>
+	);
 }
 
 export default App;
+
+
---
+++ src/component/ChatFeed.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ChatFeed(props) {
+    return (
+        <div>
+        <h2>ChatFeed</h2>
+            
+        </div>
+    );
+}
+
+export default ChatFeed;
