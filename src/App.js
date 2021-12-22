7c69841bf20647e1bc32735500ec708200ac677a
dipanshu283 <en20cs301137@medicaps.ac.in>
2021-12-22 19:54:53 +0530

    check updata

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
    <h1>hello react</h1>
+   <p>this is para for testing update check up</p>
     </div>
+    </>
   );
 }
 
